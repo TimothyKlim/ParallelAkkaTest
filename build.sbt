e4a824d0896a9d8f9667bcad823bead695424747
Timothy Klim <klimtimothy@gmail.com>
2012-08-15 03:12:22 +0400

    use after pattern

--- build.sbt
+++ build.sbt
@@ -1,13 +1,14 @@
 name := "ParallelAkkaTest"
 
-scalaVersion := "2.9.2"
+scalaVersion := "2.10.0-M6"
 
 resolvers ++= Seq(
-  "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases"
+  "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases",
+  "Typesafe Repository snaphosts" at "http://repo.typesafe.com/typesafe/snapshots"
 )
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" % "akka-actor" % "2.0.2" % "compile",
-  "com.typesafe.akka" % "akka-slf4j" % "2.0.2" % "compile",
+  "com.typesafe.akka" % "akka-actor" % "2.1-M1" % "compile",
+  "com.typesafe.akka" % "akka-slf4j" % "2.1-M1" % "compile",
   "ch.qos.logback" % "logback-classic" % "1.0.0"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,13 @@
 import akka.actor._
-import akka.dispatch.{Await, Future}
 import akka.util.Timeout
-import akka.util.duration._
+import concurrent.{Promise, Future, Await}
+import concurrent.util.Duration
+import concurrent.util.duration._
 import collection.mutable.ListBuffer
+import akka.pattern.after
+import concurrent.ExecutionContext
+import java.util.concurrent.{Executor, Executors}
+import sys.process._
 
 object Main extends App {
   implicit val system = ActorSystem("test")
@@ -12,29 +17,42 @@ object Main extends App {
   case class Done()
 
   class SlugActor(val slugID: Int) extends Actor with ActorLogging {
+    val executorThreadPool = Executors.newCachedThreadPool()
+
+    implicit val executor: Executor with ExecutionContext = ExecutionContext.fromExecutor(executorThreadPool)
+
+    override def postStop() {
+      executorThreadPool.shutdown()
+    }
+
     def receive = {
       case Work => {
-        implicit val futureTimeout = Timeout(15 seconds)
+        val senderRef = sender
 
         val future = Future {
           log.info("%d. start working" format slugID)
-          Thread.sleep(10000)
+
+          Thread.sleep(100000)
 
           true
         }
 
-        try {
-          val result = Await.result(future, futureTimeout.duration)
-
-          log.info("%d. stop working with result: %s" format(slugID, result))
-        } catch {
-          case e: java.util.concurrent.TimeoutException => log.error("%d. timeout" format slugID)
+//        Await.result(future, 1 second)
+        after(1 second, using = system.scheduler)(future) onComplete {
+          case Right(result) =>
+            log.info("%d. stop working with result: %s" format(slugID, result))
+            senderRef ! Done
+          case Left(e) =>
+            log.error("%d. timeout" format slugID)
+            senderRef ! Done
         }
-
-        sender ! Done
+//
+//        println("received")
       }
     }
   }
+
+  val actorsCount: Int = 32
 
   val hypervisor = system.actorOf(Props(new Actor {
     val slugActors = new ListBuffer[ActorRef]()
@@ -42,8 +60,8 @@ object Main extends App {
     var doneCounter = 0
 
     override def preStart() {
-      for (id <- 1 to 32) {
-        slugActors += context.actorOf(Props(new SlugActor(id)).withDispatcher("slug-actors-dispatcher"))
+      for (id <- 1 to actorsCount) {
+        slugActors += context.actorOf(Props(new SlugActor(id)))
       }
     }
 
@@ -52,7 +70,10 @@ object Main extends App {
       case Done =>
         doneCounter += 1
 
-        if (doneCounter == 32) {
+        println("doneCounter: %d" format doneCounter)
+
+        if (doneCounter == actorsCount) {
+          println("SHUTDOWN!")
           system.shutdown()
         }
     }
